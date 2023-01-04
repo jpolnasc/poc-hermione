6bf2e4bc727031a9bf409192f1498515d3acc4a4
jpolnasc <jpolnasc@gmail.com>
2023-01-04 08:07:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# poc_hermione
+
+Project started in January 03, 2023.
+
+
+**Please, complete here information on using and testing this project.**
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="poc_hermione",
+    packages=find_packages(),
+    version="0.0.1",
+)
